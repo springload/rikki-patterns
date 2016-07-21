c0f6b0a1ac537dc888f44f98582e958fd827f6b3
Josh Barr <josh@joshbarr.com>
2016-07-21 15:32:08 +1200

    ensure paths exist for component task

--- scripts/tasks/component.js
+++ scripts/tasks/component.js
@@ -12,13 +12,19 @@ const fs = require('fs');
 const del = require('del');
 
 const prefix = require('./prefix');
-
-
 const config = require('../../app/config');
 const utils = require('../../app/utils');
 
 const makeName = utils.makeName;
 const basePath = config.get('paths:components');
+
+
+const ensureDirectoryStructure = () => {
+  let yamlFile = Path.join(basePath, 'config.yaml');
+  utils.mkdirpSync('./ui');
+  utils.mkdirpSync('./ui/components');
+  fs.openSync(yamlFile, 'a');
+};
 
 
 const addToComponentsManifest = (name) => {
@@ -35,21 +41,44 @@ const addToComponentsManifest = (name) => {
     fs.writeFileSync(yamlFile, yaml.safeDump(data, dumpOpts));
 }
 
+
+const nuke = (arr, name) => {
+  let index = arr.indexOf(name);
+  if (index > -1) {
+      arr.splice(index, 1);
+  }
+  return arr;
+}
+
 const removeFromManifest = (name) => {
     let yamlFile = Path.join(basePath, 'config.yaml');
     let dumpOpts = {};
     let data = yaml.safeLoad(fs.readFileSync(yamlFile).toString()) || {};
 
     data.components = _.get(data, 'components', []);
-
-    let index = data.components.indexOf(name);
-    if (index > -1) {
-        data.components.splice(index, 1);
-    }
+    nuke(data.components, name);
 
     fs.writeFileSync(yamlFile, yaml.safeDump(data, dumpOpts));
 }
 
+
+const pruneManifest = () => {
+  let yamlFile = Path.join(basePath, 'config.yaml');
+  let data = yaml.safeLoad(fs.readFileSync(yamlFile).toString()) || {};
+  data.components = _.get(data, 'components', []);
+
+  let dirContents = fs.readdirSync(basePath);
+  let dirs = dirContents.filter(fileName => {
+    fs.statSync(Path.join(basePath, fileName)).isDirectory();
+  })
+
+  data.components
+    .filter(name => dirs.indexOf(name) < 0)
+    .forEach(name => nuke(data.components, name));
+
+  fs.writeFileSync(yamlFile, yaml.safeDump(data, {}));
+
+}
 
 const removeComponentTask = () => {
   let argv = require('yargs').argv;
@@ -58,14 +87,14 @@ const removeComponentTask = () => {
   let compPath = Path.join(basePath, component.paramName);
 
   removeFromManifest(component.paramName);
-
   gutil.log('[Deleting]', Path.join(__dirname, '..', '..', compPath));
-
   return del([compPath]);
 }
 
 
 const addComponentTask = (gulp) => {
+    ensureDirectoryStructure();
+
     let argv = require('yargs').argv;
     let name = argv.name;
     let component = makeName(name, basePath);
@@ -98,6 +127,7 @@ const addComponentTask = (gulp) => {
 
 
 module.exports = (gulp) => {
-  gulp.task(prefix('uncomponent'), () => {removeComponentTask(gulp)});
-  gulp.task(prefix('component'), () => {addComponentTask(gulp)});
+  gulp.task(prefix('uncomponent'), [prefix('components:prune')], () => {removeComponentTask(gulp)});
+  gulp.task(prefix('component'), [prefix('components:prune')], () => {addComponentTask(gulp)});
+  gulp.task(prefix('components:prune'), () => {pruneManifest()});
 }
